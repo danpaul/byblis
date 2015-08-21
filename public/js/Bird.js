5b25cf56af8d0f8c612ee1a39e7e9044392293c4
heayeon-smf <heayeon@sanbornmediafactory.com>
2015-08-21 14:51:19 -0400

    - add bomb using keyboard 'command' key - add multiple user on staging - etc

--- public/js/Bird.js
+++ public/js/Bird.js
@@ -1,4 +1,4 @@
-var Bird = function() {
+var Bird = function(app) {
 
     var bird = this;
     bird.id;
@@ -10,17 +10,45 @@ var Bird = function() {
 
     var figure;
 
+    bird.createMainBird = function(d) {
+
+    	bird.id = d.id;
+    	bird.xPosition = 30;
+    	bird.yPosition = 30;
+    	bird.name = d.name;
+    	bird.color = d.color;
+    	bird.direction = d.direction;
+
+    	var birdID="bird-"+bird.id;
+    	$('#users').append('<div class="bird" id="'+birdID+'"><div class="circle"></div><div class="triangle"></div></div>');
+
+    	figure = $('#'+birdID);
+
+    	$('#'+birdID+' .circle').css('background-color', bird.color );
+		$('#'+birdID+' .triangle').css('border-bottom-color', bird.color );
+
+		TweenMax.to( figure, 0.3, {rotation:90});
+
+    } 
+
     bird.createBird = function(d) {
 
     	bird.id = d.id;
     	bird.xPosition = d.xPosition;
     	bird.yPosition = d.yPosition;
     	bird.name = d.name;
+    	bird.color = d.color;
+    	bird.direction = d.direction;
 
     	var birdID="bird-"+bird.id;
-    	$('#canvas').append('<div class="bird" id="'+birdID+'"><div class="circle"></div><div class="triangle"></div></div>')
+    	$('#canvas').append('<div class="bird" id="'+birdID+'"><div class="circle"></div><div class="triangle"></div></div>');
 
     	figure = $('#'+birdID);
+
+    	$('#'+birdID+' .circle').css('background-color', bird.color );
+		$('#'+birdID+' .triangle').css('border-bottom-color', bird.color );
+
+		TweenMax.to( figure, 0.3, {rotation:bird.direction, top:bird.xPosition, left:bird.yPosition});
     } 
 
     bird.updatePosition = function(d) {
@@ -35,14 +63,17 @@ var Bird = function() {
 			tx = a+p.left;
 			if(a<0){r=-90} else {r=90}
 			TweenMax.to(figure, 0.4, {left:tx+'px', rotation:r, ease:Power2.easeOut, overwrite:1});
-			bird.xPosition = tx;
+			app.model.mainBird.xPosition = tx;
 		}
 		else {
 			ty = a+p.top;
 			if(a<0){r=0} else {r=180}
 			TweenMax.to(figure, 0.4, {top:ty+'px', rotation:r, ease:Power2.easeOut, overwrite:1});
-			bird.yPosition = ty;
+			app.model.mainBird.yPosition = ty;
 		}
+
+		app.model.mainBird.direction = r;
+		app.control.sendUpdatedUser();
     }
 
 	// (function(){	
--- public/js/Control.js
+++ public/js/Control.js
@@ -3,11 +3,17 @@ var Control = function(a, s) {
 	var control = this;
 	var app = a;
 	var socket = s;
+	var init = false;
 
 	socket.on('news', function (data) {
         console.log(data);
         socket.emit('my other event', { my: 'data' });
     });
+
+	control.sendUpdatedUser = function() {
+		// console.log(app.model.mainBird)
+		socket.emit('userUpdatePostion', app.model.mainBird);
+	}
 
     socket.on('connect', function (data) {
         console.log('in connect event');
@@ -17,40 +23,56 @@ var Control = function(a, s) {
                 return;
             }
 
-            console.log(data.user);
-            var b = new Bird();
-            b.createBird(data.user);
+            console.log(data);
 
-            app.model.mainBird = b;
+            if( init == false )
+            {        	
+	            var b = new Bird(app);
+	            b.createMainBird(data.user);
+	            app.model.mainBird = b;
+
+	            init = true;
+            }
+
+            var u = data.gameSate.users;
 
 
-// data will looks like this:
-// {  
-//     "user":{  
-//         "id":"754c8930-4828-11e5-b242-5ff81680b6ad",
-//         "name":"Bowleopard Gorilla",
-//         "xPosition":7579,
-//         "yPosition":1523
-//     },
-//     "gameSate":{  
-//         "time":null,
-//         "users":[  
+            for( var i=0; i<u.length; i++ )
+            {
+            	var otherBird = new Bird(app);
+            	otherBird.createBird(u[0]);
+            }
 
-//         ],
-//         "trees":[  
+			// data will looks like this:
+			// {  
+			//     "user":{  
+			//         "id":"754c8930-4828-11e5-b242-5ff81680b6ad",
+			//         "name":"Bowleopard Gorilla",
+			//         "xPosition":7579,
+			//         "yPosition":1523
+			//     },
+			//     "gameSate":{  
+			//         "time":null,
+			//         "users":[  
 
-//         ],
-//         "bombs":[  
+			//         ],
+			//         "trees":[  
 
-//         ]
-//     }
-// }
+			//         ],
+			//         "bombs":[  
 
+			//         ]
+			//     }
+			// }
         })
+
+
+
+
+
 
     });
 
-	// (function(){	
-	// })();
+
 
 }
